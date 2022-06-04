5ab78204bcb65f4535bbb4848d3422ba8b6f5bd7
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-06-05 00:59:43 +0530

    Set Matrix Zeros optimised Approach

--- Arrays Part I/Set_Matrix_Zeros.cpp
+++ Arrays Part I/Set_Matrix_Zeros.cpp
@@ -28,3 +28,40 @@ void setZeros(vector<vector<int>> &matrix)
     }
     Setrowsandcoltozero(matrix,rows,cols);
 }
+
+// Optimised Approach 
+void setZeros(vector<vector<int>> &matrix)
+{
+    bool row=false,col=false; // marked for 0,0 element
+    for(int i=0;i<matrix.size();i++)
+    {
+        for(int j=0;j<matrix[0].size();j++)
+        {
+            if(matrix[i][j]==0)
+            {
+                if(j==0)
+                    col=true;
+                if(i==0)
+                    row=true;
+                matrix[0][j]=0;
+                matrix[i][0]=0;
+            }
+        }
+    }
+    for(int i=1;i<matrix.size();i++)
+    {
+        for(int j=1;j<matrix[0].size();j++)
+        {
+            if(matrix[i][0]==0 || matrix[0][j]==0)
+                matrix[i][j]=0;
+        }
+    }
+    if(row)
+        for(int i=0;i<matrix[0].size();i++)
+            matrix[0][i]=0;
+    if(col)
+        for(int i=0;i<matrix.size();i++)
+            matrix[i][0]=0;
+            
+   
+}
