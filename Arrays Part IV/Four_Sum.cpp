dd94b692f9c1f24c84f046313c625f2491b422ea
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-06-17 19:11:10 +0530

    Four Sum Updated

--- Arrays Part IV/Four_Sum.cpp
+++ Arrays Part IV/Four_Sum.cpp
@@ -1,34 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
-class Solution {
-public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-       vector<vector<int>> v;
-        if(nums.size()<4) return v;
-        sort(nums.begin(), nums.end());
-        for(int i=0;i<nums.size()-3;i++){
-            if(i!=0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<nums.size()-2;j++){
-                if(j!=i+1 && nums[j]==nums[j-1]) continue;
-                int start=j+1;
-                int end=nums.size()-1;
-                while(start<end){
-                    long long int sum=(long)nums[i]+(long)nums[j]+(long)nums[start]+(long)nums[end];
-                    if(sum==target) 
-                    {
-                        v.push_back({nums[i], nums[j], nums[start], nums[end]});
-                        while(start<end && nums[end]==nums[end-1]) end--;
-                        while(start<end && nums[start]==nums[start+1]) start++;
-                        start++;
-                        end--;
-                    }
-                    else if(sum<target) start++;
-                    else{
-                        end--;
-                    }
-                }
+string fourSum(vector<int> arr, int target, int n) {
+    // Write your code here.
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<arr.size()-1;i++)
+    {
+        if(i!=0 && arr[i]==arr[i-1]) continue;
+        for(int j=i+1;j<arr.size();j++)
+        {
+            if(j!=i+1 && arr[j]==arr[j-1]) continue;
+            int start=j+1,end=arr.size()-1;
+            while(start<end)
+            {
+                int sum=arr[i]+arr[j]+arr[start]+arr[end];
+                if(sum>target)
+                    end--;
+                else if(sum<target)
+                    start++;
+                else
+                    return "Yes";
             }
         }
-        return v;
     }
-};
+    return "No";
+}
