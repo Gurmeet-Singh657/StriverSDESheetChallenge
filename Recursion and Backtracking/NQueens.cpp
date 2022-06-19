2c3ab411ea9ebf2f1bd2753f50d64fcac4c67205
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-06-20 02:24:32 +0530

    N Queens Modified

--- Recursion and Backtracking/NQueens.cpp
+++ Recursion and Backtracking/NQueens.cpp
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
 bool isSafe(int i,int j,vector<vector<int>>& chessboard)
 {
     for(int row=0;row<chessboard.size();row++)
@@ -58,5 +60,48 @@ vector<vector<int>> solveNQueens(int n) {
     vector<vector<int>> chessboard(n,vector<int>(n,0));
     vector<vector<int>> ans;
     NQueen(chessboard,0,ans);
+    return ans;
+}
+
+// Optimised Approach
+void NQueen(vector<vector<int>>& chessboard,int ind,vector<vector<int>>& ans,vector<int>& checkcol,vector<int>& leftdiagonal,vector<int>& rightdiagonal)
+{
+    if(ind==chessboard.size())
+    {
+        vector<int> finalans;
+        for(int i=0;i<chessboard.size();i++)
+        {
+            for(int j=0;j<chessboard.size();j++)
+            {
+                finalans.push_back(chessboard[i][j]);
+            }
+        }
+        ans.push_back(finalans);
+        return;
+    }
+    for(int j=0;j<chessboard.size();j++)
+    {
+        if(checkcol[j]==0 && leftdiagonal[ind+j]==0 && rightdiagonal[chessboard.size()-1+j-ind]==0)
+        {
+            chessboard[ind][j]=1;
+            checkcol[j]=1;
+            leftdiagonal[ind+j]=1;
+            rightdiagonal[chessboard.size()-1+j-ind]=1;
+            NQueen(chessboard,ind+1,ans,checkcol,leftdiagonal,rightdiagonal);
+            checkcol[j]=0;
+            leftdiagonal[ind+j]=0;
+            rightdiagonal[chessboard.size()-1+j-ind]=0;
+            chessboard[ind][j]=0;
+        }
+    }
+}
+vector<vector<int>> solveNQueens(int n) {
+    // Write your code here.
+    vector<vector<int>> chessboard(n,vector<int>(n,0));
+    vector<vector<int>> ans;
+    vector<int> checkcol(n,0);
+    vector<int> leftdiagonal(2*n-1,0);
+    vector<int> rightdiagonal(2*n-1,0);
+    NQueen(chessboard,0,ans,checkcol,leftdiagonal,rightdiagonal);
     return ans;
 }
