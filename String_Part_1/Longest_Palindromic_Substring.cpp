b6757eb49ceb1f6a383be1aad481ae4a9318fa1b
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-07-05 01:41:08 +0530

    Longest Palindromic Substring

---
+++ String_Part_1/Longest_Palindromic_Substring.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+string longestPalinSubstring(string str)
+{
+    string longest=str.substr(0,1);
+    int maxlen=1;
+    int minindex=0;
+    for(int i=0;i<str.length();i++)
+    {
+        // Checking odd lengths
+        int start=i,end=i;
+        while(start>=0 && end<str.length())
+        {
+            if(str[start]!=str[end])
+              break;
+            if(end-start+1>maxlen)
+            {
+                longest=str.substr(start,end-start+1);
+                maxlen=end-start+1;
+                minindex=start;
+            }
+            else if(end-start+1==maxlen && minindex>start)
+            {
+                longest=str.substr(end-start+1);
+                minindex=start;
+            }
+            start--;
+            end++;
+        }
+        // Checking for Even lengths
+         start=i,end=i+1;
+        while(start>=0 && end<str.length())
+        {
+            if(str[start]!=str[end])
+              break;
+            if(end-start+1>maxlen)
+            {
+                longest=str.substr(start,end-start+1);
+                maxlen=end-start+1;
+                minindex=start;
+            }
+            else if(end-start+1==maxlen && minindex>start)
+            {
+                longest=str.substr(end-start+1);
+                minindex=start;
+            }
+            start--;
+            end++;
+        }
+    }
+    return longest;
+}
