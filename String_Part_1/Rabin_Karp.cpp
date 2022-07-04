ec2a7aec808674a6fd7caa2bb38b308ce4a10aa6
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-07-05 05:12:59 +0530

    Rabin Karp Updated

--- String_Part_1/Rabin_Karp.cpp
+++ String_Part_1/Rabin_Karp.cpp
@@ -1,36 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int base=31;
-const int primeno=29;
-// Submission on Coding Ninjas
-#include<bits/stdc++.h>
-const int BASE = 1000000;
-vector<int> stringMatch(string& source, string& target) {
+vector<int> stringMatch(string &str, string &pat) {
+    int primeno=1000000;
     vector<int> ans;
-     if(source == "" or target == "") return ans;
-        int m = target.size();
-        int power = 1;
-        for(int i = 0;i<m;i++){
-            power = (power*31)%BASE;
+    int store=0;
+    int power = 1;
+        for(int i = 0;i<pat.length();i++){
+            power = (power*31)%primeno;
         }
-        int targetCode = 0;
-        for(int i = 0;i<m;i++){
-            targetCode = (targetCode*31+target[i])%BASE;
+    for(int i=0;i<pat.length();i++)
+    {
+        store=(store*31+pat[i])%primeno;
+    }
+    int currans=0;
+    for(int i=0;i<str.length();i++)
+    {
+        currans=(currans*31+str[i])%primeno;
+        if(i<pat.length()-1)
+            continue;
+        
+        if(i>=pat.length())
+        {
+            currans=(currans-str[i-pat.length()]*power)%primeno;
         }
-        int hashCode = 0;
-        for(int i = 0;i<source.size();i++){
-            hashCode = (hashCode*31 + source[i])%BASE;
-            if(i<m-1) continue;
-            if(i>=m){
-                hashCode = (hashCode-source[i-m]*power)%BASE;
-            }
-            if(hashCode<0)
-                hashCode+=BASE;
-            if(hashCode == targetCode){
-                if(source.substr(i-m+1,m) == target)
-                    ans.push_back(i-m+1);
-            }
-        }
-        return ans;
+        if(currans<0)
+            currans=currans+primeno;
+        if(currans==store)
+            ans.push_back(i-pat.length()+1);
+    }
+    return ans;
 }
-
