53b182f90406a56ac91c7badace09cf2f72a5392
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-07-05 01:25:15 +0530

    Reverse String

---
+++ String_Part_1/Reverse_String.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+string reverseString(string str)
+{
+    string ans;
+    for(int i=str.length()-1;i>=0;i--)
+    {
+        if(str[i]==' ')
+            continue;
+        int j=i;
+        while(j>=0 && str[j]!=' ')
+            j--;
+        ans+=str.substr(j+1,i-j);
+        ans+=' ';
+        i=j;
+    }
+    return ans;
+}
