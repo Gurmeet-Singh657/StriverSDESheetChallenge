4d7d9c64dd9326c2512c4669438949f61fb70980
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-07-08 00:05:02 +0530

    Inorder Traversal & Preorder Traversal Update

--- Binary_Tree/Inorder_Traversal.cpp
+++ Binary_Tree/Inorder_Traversal.cpp
@@ -24,3 +24,39 @@ vector<int> getInOrderTraversal(TreeNode *root)
     InorderTraversal(root,ans);
     return ans;
 }
+
+// Iterative Inorder Traversal
+#include<bits/stdc++.h>
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    if(root==NULL)
+        return ans;
+    TreeNode* node=root;
+    stack<TreeNode*> st;
+    while(node!=NULL || !st.empty())
+    {
+        while(node!=NULL)
+        {
+            st.push(node);
+            node=node->left;
+        }
+        node=st.top();
+        st.pop();
+        ans.push_back(node->data);
+        node=node->right;
+     }
+    return ans;
+}
--- Binary_Tree/Preorder_Traversal.cpp
+++ Binary_Tree/Preorder_Traversal.cpp
@@ -24,3 +24,74 @@ vector<int> getPreOrderTraversal(TreeNode *root)
     PreorderTraversal(root,ans);
     return ans;
 }
+
+// Iterative Preorder Traversal
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+vector<int> getPreOrderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    stack<TreeNode*> st;
+    TreeNode* curr=root;
+    while(curr!=NULL || !st.empty())
+    {
+        while(curr!=NULL)
+        {
+            ans.push_back(curr->data);
+            st.push(curr);
+            curr=curr->left;
+        }
+        curr=st.top();
+        st.pop();
+        curr=curr->right;
+    }
+    return ans;
+}
+
+
+// Striver soln
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+vector<int> getPreOrderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    if(root==NULL)
+        return ans;
+    stack<TreeNode*> st;
+    st.push(root);
+    while(!st.empty())
+    {
+        TreeNode* curr=st.top();
+        ans.push_back(curr->data);
+        st.pop();
+        if(curr->right!=NULL)
+        {
+            st.push(curr->right);
+        }
+        if(curr->left!=NULL)
+        {
+            st.push(curr->left);
+        }
+    }
+    return ans;
+}
