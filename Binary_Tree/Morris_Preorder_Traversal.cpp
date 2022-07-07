f95874bf2208feaa46955ab059c42088fb9c04a7
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-07-08 03:00:00 +0530

    Morris Preorder Traversal

---
+++ Binary_Tree/Morris_Preorder_Traversal.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode* curr=root;
+        while(curr!=NULL)
+        {
+            if(curr->left) // if left subtree exists
+            {
+                TreeNode* temp=curr->left;
+                while(temp->right!=NULL && temp->right!=curr)
+                {
+                    temp=temp->right;
+                }
+                if(temp->right==curr) // means already threaded -> remove thread and go to right
+                {
+                    temp->right=NULL;
+                    curr=curr->right;
+                }
+                else // thread it , print it and go to left
+                {
+                    ans.push_back(curr->val);
+                    temp->right=curr;
+                    curr=curr->left;
+                } 
+            }
+            else // if left subtree does not exists
+            {
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+        }
+        return ans;
+    }
+};
