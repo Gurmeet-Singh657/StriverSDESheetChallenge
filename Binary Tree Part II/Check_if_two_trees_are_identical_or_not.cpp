58dc4f1adc529f24fec1f71bb5bd315cefa01918
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-07-10 02:40:10 +0530

    Check if two trees are identical or not

---
+++ Binary Tree Part II/Check_if_two_trees_are_identical_or_not.cpp
@@ -0,0 +1,30 @@
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+
+    class BinaryTreeNode {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+		
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+***********************************************************/
+
+bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+    if(root1==NULL && root2==NULL)
+        return true;
+    if(root1==NULL || root2==NULL || root1->data!=root2->data)
+        return false;
+    bool leftans=identicalTrees(root1->left,root2->left);
+    bool rightans=identicalTrees(root1->right,root2->right);
+    return leftans & rightans;
+}
