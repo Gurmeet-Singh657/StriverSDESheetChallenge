238a5de2cfd35e529e82d48ce87a78b17c168686
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-07-19 21:16:09 +0530

    Children Sum Property

---
+++ Binary Tree Part III/Children_sum_property.cpp
@@ -0,0 +1,44 @@
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode < T > *left;
+        BinaryTreeNode < T > *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+void changeTree(BinaryTreeNode < int > * root) {
+    if(root==NULL || (!root->left && !root->right))
+        return;
+    int check=0;
+    if(root->left)
+        check+=root->left->data;
+    if(root->right)
+        check+=root->right->data;
+    if(check>=root->data)
+        root->data=check;
+    else 
+    {
+        if(root->left) root->left->data=root->data;
+        if(root->right) root->right->data=root->data;
+    }
+    changeTree(root->left);
+    changeTree(root->right);
+    int total=0;
+    if(root->left)
+        total+=root->left->data;
+    if(root->right)
+        total+=root->right->data;
+    if(total!=0)
+        root->data=total;
+}  
