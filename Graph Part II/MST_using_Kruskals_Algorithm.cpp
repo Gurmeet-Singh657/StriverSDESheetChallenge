1e84d286ed267999450f46515b6dd57c5cfbd1a4
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2022-07-26 00:49:33 +0530

    MST using Kruskals Algo - return weight of MST

--- Graph Part II/MST_using_Kruskals_Algorithm.cpp
+++ Graph Part II/MST_using_Kruskals_Algorithm.cpp
@@ -52,3 +52,59 @@ vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pa
     }
     return ans;
 }
+
+
+// Finding the Weight
+#include<bits/stdc++.h>
+using namespace std;
+int findparent(int u,vector<int>& parent)
+{
+    if(parent[u]==u)
+        return u;
+    return parent[u]=findparent(parent[u],parent);
+}
+void Union(int u,int v,vector<int>& parent,vector<int>& rank)
+{
+    u=findparent(u,parent);
+    v=findparent(v,parent);
+    if(rank[u]<rank[v])
+    {
+        parent[u]=v;
+    }
+    else if(rank[v]<rank[u])
+    {
+        parent[v]=u;
+    }
+    else
+    {
+        parent[v]=u;
+        rank[u]++;
+    }
+}
+bool cmp(vector<int>& p1,vector<int>& p2)
+{
+    return p1[2]<p2[2];
+}
+int kruskalMST(int n, int m, vector<vector<int>> &g) {
+    int ans=0;
+    vector<int> parent(n+1);
+    vector<int> rank(n+1);
+    for(int i=1;i<=n;i++)
+    {
+        parent[i]=i;
+        rank[i]=0;
+    }
+    sort(g.begin(),g.end(),cmp);
+    for(int i=0;i<g.size();i++)
+    {
+        int u=g[i][0];
+        int v=g[i][1];
+        int wt=g[i][2];
+        if(findparent(u,parent)!=findparent(v,parent))
+        {
+            Union(u,v,parent,rank);
+            ans+=wt;
+        }
+    }
+    return ans;
+}
